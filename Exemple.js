c650fdd6b20253a18002e50d49a8e11f7d5b85ea
Titi <95020920+Tristitititi@users.noreply.github.com>
2022-03-01 16:57:14 +0100

    Exercice

---
+++ Exemple.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    var year = 2022;
+
+    //year % 4 === 0 && year % 100 > 0 || year % 400 === 0 ? console.log(year + ' est bissextile') : console.log(year + ' n\'est pas bissextile');
+
+    /*if ((year % 4 === 0 && year % 100 > 0) || (year % 400 === 0)) {
+        console.log('Oui');
+    } else {
+        console.log('Non');
+    }*/
+
+    /*var number = 42;
+
+    var units = number % 10;
+    var tens = (number - units) / 10; il suffit de soustraire les unités puis de diviser par 10.*/
+});
